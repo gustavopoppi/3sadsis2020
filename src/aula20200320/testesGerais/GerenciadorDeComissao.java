209aaabdadfa7114dc3410ed00d453bbc44ee6f0
Gustavo Poppi <48832984+gustavopoppi@users.noreply.github.com>
2020-03-20 20:13:12 -0300

    Update GerenciadorDeComissao.java
    Adicionando métodos que estavam faltando

--- src/aula20200320/testesGerais/GerenciadorDeComissao.java
+++ src/aula20200320/testesGerais/GerenciadorDeComissao.java
@@ -1,9 +1,31 @@
-package aula20200320.testesGerais;
+package aula20032020.testesGerais;
 
 public class GerenciadorDeComissao {
-	
+
 	public static double calcularComissaoPadrao(double valorDaVenda, double percentualDeComissao) {
-		return valorDaVenda * (percentualDeComissao/100.00);
+		return valorDaVenda * (percentualDeComissao / 100.00);
 	}
 
+	public static double calcularComissaoComValorMinimo(double valorMinimo, double valorDaVenda,
+			double percentualDeComissao) {
+		double comissao = valorDaVenda * (percentualDeComissao / 100.00);
+		if (comissao < valorMinimo) {
+			return comissao;
+		}
+		return comissao;
+	}
+
+	public static double calcularComissaoComFaixa(double valorMinimo, double valorMaximo, double valorDaVenda,
+			double percentualDeComissao) {
+
+		double comissao = valorDaVenda * (percentualDeComissao / 100.00);
+		
+		if (comissao < valorMinimo) {
+			return valorMinimo;
+		} else if (comissao > valorMaximo) {
+			return valorMaximo;
+		}
+		return comissao;
+	}
 }
+
