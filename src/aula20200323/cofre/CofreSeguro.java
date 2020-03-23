819b46d43f0208e52e0b048e22512f763d25b206
Gustavo Poppi <gustavopoppi@hotmail.com>
2020-03-23 19:59:37 -0300

    Arrumando identação do código

--- src/aula20200323/cofre/CofreSeguro.java
+++ src/aula20200323/cofre/CofreSeguro.java
@@ -2,34 +2,35 @@ package aula20200323.cofre;
 
 public class CofreSeguro {
 
-	/* Elabore uma segunda classe, denominada CofreSeguro,
-	que possua uma SENHA numérica definida pelo construtor e que só seja aberto se a senha correta for informada.*/
-	
+	/*
+	 * Elabore uma segunda classe, denominada CofreSeguro, que possua uma SENHA
+	 * numérica definida pelo construtor e que só seja aberto se a senha correta for
+	 * informada.
+	 */
+
 	private boolean aberto;
 	private int senha;
-	
-	public CofreSeguro(int senha){
+
+	public CofreSeguro(int senha) {
 		this.senha = senha;
 		this.aberto = false;
 	}
-	
+
 	public void Abrir(int senha) {
 		if (this.senha == senha) {
 			aberto = true;
-		}
-		else {
+		} else {
 			System.out.println("Senha incorreta");
-			aberto = false;			
+			aberto = false;
 		}
 	}
-	
+
 	public void Fechar() {
 		aberto = false;
 	}
-	
+
 	public boolean isAberto() {
 		return aberto;
 	}
-	
-	
+
 }
