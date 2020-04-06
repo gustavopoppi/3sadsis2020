a203cd2d94a174d207e240b0a0e4a2681a8eb048
Gustavo Poppi <gustavopoppi@hotmail.com>
2020-04-06 20:33:39 -0300

    Ajuste construtor

--- src/aula20200406/tiposDeMetodos/Pessoa.java
+++ src/aula20200406/tiposDeMetodos/Pessoa.java
@@ -9,10 +9,10 @@ public class Pessoa {
 	private double altura;
 
 	public Pessoa(String nome, int idade, double peso, double altura) {
-		this.nome = nome;
-		this.idade = idade;
-		this.peso = peso;
-		this.altura = altura;
+		setNome(nome);
+		setIdade(idade);
+		setPeso(peso);
+		setAltura(altura);
 	}
 	
 	public double getIMC() {
