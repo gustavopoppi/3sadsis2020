9534244ad57a75da23b60d54ba2de7d36ccd5b74
Gustavo Poppi <48832984+gustavopoppi@users.noreply.github.com>
2020-05-11 21:10:29 -0300

    Ajuste indentação código

--- src/aula20200511/fundamentosColecoes/atividade/App.java
+++ src/aula20200511/fundamentosColecoes/atividade/App.java
@@ -7,7 +7,7 @@ public class App {
 	public static void main(String[] args) {
 		Produto omo = new Produto("Omo");
 		Produto coca = new Produto("Coca");
-		Produto feijao = new Produto("Feij„o");
+		Produto feijao = new Produto("Feij√£o");
 		
 		ItemPedido itemUm = new ItemPedido(omo, 1, 1);
 		ItemPedido itemDois = new ItemPedido(coca, 1.5, 6.2);
@@ -15,14 +15,14 @@ public class App {
 		Pedido pedidoUm = new Pedido(1);
 		pedidoUm.adicionarItem(itemUm);
 		pedidoUm.adicionarItem(itemDois);
-		System.out.println(pedidoUm.getItensPedido());
+		//System.out.println(pedidoUm.getItensPedido());
 
 		for (int i = 0; i < pedidoUm.getItensPedido().size(); i++) {
 			ItemPedido e = pedidoUm.getItensPedido().get(i);
 			System.out.println("Pedido " + (i+1) + 
-					           ", produto: " + e.getProduto().getNome() + 
-					           ", quantidade: "+ e.getQuantidade() +
-							   ", valor: R$" + e.getPreco());
+					   ", produto: " + e.getProduto().getNome() + 
+					   ", quantidade: "+ e.getQuantidade() +
+					   ", valor: R$" + e.getPreco());
 		}
 	}
 
