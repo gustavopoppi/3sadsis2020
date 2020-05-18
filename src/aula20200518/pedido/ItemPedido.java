932237a584914d9b2cd7e541c9abad7137514620
Gustavo Poppi <gustavopoppi@hotmail.com>
2020-05-18 20:16:20 -0300

    Adicionado o método getValorTotal na classe ItemPedido

--- src/aula20200518/pedido/ItemPedido.java
+++ src/aula20200518/pedido/ItemPedido.java
@@ -1,35 +1,39 @@
 package aula20200518.pedido;
 
 public class ItemPedido {
-	// - quantidade : double
+	//- quantidade : double
 	private double quantidade;
-
-	// - preco : double
+	
+	//- preco : double
 	private double preco;
-
-	// - produto (atributo de associação)
+	
+	//- produto (atributo de associação) 
 	private Produto produto;
-
-	// + getQuantidade() : double
+	
+	//+ getQuantidade() : double
 	public double getQuantidade() {
 		return quantidade;
 	}
-
-	// + getpreco() : double
+	
+	//+ getpreco() : double
 	public double getPreco() {
 		return preco;
 	}
-
-	// + getProduto() : Produto
+	
+	//+ getProduto() : Produto
 	public Produto getProduto() {
 		return produto;
 	}
-
-	// + ItemPedido(produto : Produto, quantidade : double, preco : double)
+	
+	//+ ItemPedido(produto : Produto, quantidade : double, preco : double)
 	public ItemPedido(Produto produto, double quantidade, double preco) {
 		this.produto = produto;
 		this.quantidade = quantidade;
 		this.preco = preco;
+	}
+	
+	public double getValorTotal() {
+		return this.preco * this.quantidade;
 	}
 
 	@Override
@@ -65,5 +69,4 @@ public class ItemPedido {
 			return false;
 		return true;
 	}
-
 }
